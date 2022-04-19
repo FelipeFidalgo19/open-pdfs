f441b866fa048c672d3e1d8b605cc438ed09a2b0
FelipeFidalgo19 <felipefidalgo100@gmail.com>
2022-04-19 09:19:02 -0300

    1 binaries uploaded

--- src/View/App.js
+++ src/View/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Pdf from 'modelo.pdf';
 import './style.css';
 
 export default function App() {
+  function onResumeClick() {
+    window.open(Pdf);
+  }
   return (
     <div>
       <h1>muda!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div>
+        <a onClick={this.onResumeClick}>Resume</a>
+      </div>
     </div>
   );
 }
