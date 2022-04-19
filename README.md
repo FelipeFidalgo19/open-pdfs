a730153afdc4eba4ba257888997ce4bc741d92e2
FelipeFidalgo19 <felipefidalgo100@gmail.com>
2022-04-19 09:04:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-waqg26
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-waqg26)
---
+++ src/View/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>teste!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
